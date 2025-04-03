52feed47faa5ccbd1c316069c641c710ad194931
Umoren <samuelumoren365@gmail.com>
2025-04-03 13:50:16 +0100

    fix remote sdk issue

--- lib/resend.ts
+++ lib/resend.ts
@@ -1,3 +1,3 @@
 import { Resend } from 'resend';
 
-export const RESEND_SDK = new Resend(process.env.NEST_PUBLIC_RESEND_API_KEY);
+export const RESEND_SDK = new Resend(process.env.NEXT_PUBLIC_RESEND_API_KEY);
